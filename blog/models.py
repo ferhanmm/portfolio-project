59ec982e7b0ac645a8167b4048c5ad871ee705fd
ferhanmm <ferhanmm@gmail.com>
2020-11-16 17:01:56 -0600

    Polish

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,17 @@
 from django.db import models
 from datetime import date
 
-# Create your models here.
-#title
-#pub_date
-#body
-#image
-
-# create a blog models
-
-# add the blog app to the settings
-
-# create a migration
-
-# migrate
-
-# add to the admin
-
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to= 'images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
